3d44321803bd2a8295887e47a8fa843254f5c77d
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-28 21:01:59 +0500

    draw background of emotions

--- app/src/main/java/xyz/teamgravity/customviewemotions/EmotionalFaceView.kt
+++ app/src/main/java/xyz/teamgravity/customviewemotions/EmotionalFaceView.kt
@@ -1,8 +1,51 @@
 package xyz.teamgravity.customviewemotions
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    private var faceColor = Color.YELLOW
+    private var eyesColor = Color.BLACK
+    private var mouthColor = Color.BLACK
+    private var borderColor = Color.BLACK
+    private var borderWidth = 4.0F
+    private var size = 320
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas) // keep any drawing from the parent side
+
+        drawFaceBackground(canvas)
+        drawEyes(canvas)
+        drawMouth(canvas)
+    }
+
+    private fun drawFaceBackground(canvas: Canvas) {
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
+
+        val radius = size / 2F
+
+        canvas.drawCircle(size / 2F, size / 2F, radius, paint)
+
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderWidth
+
+        canvas.drawCircle(size / 2F, size / 2F, radius, paint)
+    }
+
+    private fun drawEyes(canvas: Canvas) {
+
+    }
+
+    private fun drawMouth(canvas: Canvas) {
+
+    }
 }
