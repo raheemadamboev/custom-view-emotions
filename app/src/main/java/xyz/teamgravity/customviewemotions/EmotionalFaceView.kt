8033052e7cb5a9a2692f2ac6d545713e89f25ddf
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-28 20:47:37 +0500

    create basic custom view and place it in layout

---
+++ app/src/main/java/xyz/teamgravity/customviewemotions/EmotionalFaceView.kt
@@ -0,0 +1,8 @@
+package xyz.teamgravity.customviewemotions
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+}
