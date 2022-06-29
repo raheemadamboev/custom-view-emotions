67ee1a2d5b3299d849ae84e011414d149ba25cd6
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-29 13:06:43 +0500

    draw the happy face and measure the size
    Took 30 minutes

--- app/src/main/java/xyz/teamgravity/customviewemotions/EmotionalFaceView.kt
+++ app/src/main/java/xyz/teamgravity/customviewemotions/EmotionalFaceView.kt
@@ -1,15 +1,14 @@
 package xyz.teamgravity.customviewemotions
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
 class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val mouthPath = Path()
 
     private var faceColor = Color.YELLOW
     private var eyesColor = Color.BLACK
@@ -17,6 +16,12 @@ class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, a
     private var borderColor = Color.BLACK
     private var borderWidth = 4.0F
     private var size = 320
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        size = measuredWidth.coerceAtMost(measuredHeight)
+        setMeasuredDimension(size, size)
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas) // keep any drawing from the parent side
@@ -27,25 +32,37 @@ class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, a
     }
 
     private fun drawFaceBackground(canvas: Canvas) {
-        paint.color = faceColor
-        paint.style = Paint.Style.FILL
-
         val radius = size / 2F
 
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
         canvas.drawCircle(size / 2F, size / 2F, radius, paint)
 
         paint.color = borderColor
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = borderWidth
-
         canvas.drawCircle(size / 2F, size / 2F, radius, paint)
     }
 
     private fun drawEyes(canvas: Canvas) {
+        paint.color = eyesColor
+        paint.style = Paint.Style.FILL
 
+        val leftEyeRect = RectF(size * 0.32F, size * 0.23F, size * 0.43F, size * 0.5F)
+        canvas.drawOval(leftEyeRect, paint)
+
+        val rightEyesRect = RectF(size * 0.57F, size * 0.23F, size * 0.68F, size * 0.5F)
+        canvas.drawOval(rightEyesRect, paint)
     }
 
     private fun drawMouth(canvas: Canvas) {
+        mouthPath.moveTo(size * 0.22F, size * 0.7F)
+        mouthPath.quadTo(size * 0.5F, size * 0.8F, size * 0.78F, size * 0.7F)
+        mouthPath.quadTo(size * 0.5F, size * 0.9F, size * 0.22F, size * 0.7F)
 
+        paint.color = mouthColor
+        paint.style = Paint.Style.FILL
+
+        canvas.drawPath(mouthPath, paint)
     }
 }
