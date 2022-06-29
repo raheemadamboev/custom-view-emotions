9294799e1c7bc73772ac7aa491860ce58f5dd1a5
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-29 14:35:25 +0500

    setup viewbinding
    Took 1 minute

--- app/src/main/java/xyz/teamgravity/customviewemotions/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/customviewemotions/MainActivity.kt
@@ -1,12 +1,16 @@
 package xyz.teamgravity.customviewemotions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import xyz.teamgravity.customviewemotions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
